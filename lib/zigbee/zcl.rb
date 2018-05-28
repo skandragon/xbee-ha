d337c965c572c4f0c663197f3886ae306e3574dc
Michael Graff <michael.graff@nominum.com>
2018-05-28 14:44:44 -0700

    sort

--- lib/zigbee/zcl.rb
+++ lib/zigbee/zcl.rb
@@ -1,9 +1,11 @@
 require_relative 'zcl/profiles'
+
 require_relative 'zcl/frame_control_field'
 require_relative 'zcl/header'
+
+require_relative 'zcl/configure_cluster_reporting'
 require_relative 'zcl/default_response'
 require_relative 'zcl/read_attributes'
 require_relative 'zcl/read_attributes_response'
 require_relative 'zcl/write_attributes'
 require_relative 'zcl/write_attributes_response'
-require_relative 'zcl/configure_cluster_reporting'
